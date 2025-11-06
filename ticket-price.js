1743786618e96eec269cf9798652f04c1dd6182e
you <you@gmail.com>
2025-11-06 10:26:34 +0200

    third

---
+++ ticket-price.js
@@ -0,0 +1,10 @@
+let visitorAge = 12;
+let isStudent = true;
+if(visitorAge <= 6){
+    console.log("Biļete ir par brīvu.")
+}else if(isStudent){
+    console.log("Biļete ir par 5 EUR.")
+}
+else{
+    console.log("Biļete ir par 10 EUR.")
+}
